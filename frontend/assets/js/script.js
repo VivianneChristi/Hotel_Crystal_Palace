e956af884dd485d0f9771f55556d2e2874f37408
Vivianne Christi <viviannec.mferreira@gmail.com>
2024-06-11 23:24:42 -0300

    comittt

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -41,11 +41,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
             var quartosDisponiveis = [
                 { nome: "Quarto Single", capacidade: "1", camas: "1 cama de solteiro" },
-                {nome: "Quarto Friends", capacidade: "2", camas: "2 camas de solteiro"},
+                { nome: "Quarto Friends", capacidade: "2", camas: "2 camas de solteiro" },
                 { nome: "Quarto Duplo", capacidade: "2", camas: "1 cama de casal" },
-                { nome: "Quarto Triplo", capacidade: "3", camas: "1 cama de casal, 1 cama de solteiro" },
+                { nome: "Quarto Triplo", capacidade: "3", camas: "1 cama de casal <br> 1 cama de solteiro" },
                 { nome: "Suíte Familiar", capacidade: "4", camas: "2 camas de casal" },
-                { nome: "Cobertura Luxo", capacidade: "5+", camas: "2 camas de casal, 3 camas de solteiro" }
+                { nome: "Cobertura Luxo", capacidade: "5+", camas: "2 camas de casal<br> 3 camas de solteiro" }
             ];
 
             var resultado = quartosDisponiveis.filter(quarto => {
@@ -57,26 +57,34 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
             var resultadoBuscaDiv = document.getElementById("resultado_busca");
-            resultadoBuscaDiv.innerHTML = "";
+            resultadoBuscaDiv.innerHTML = ""; // Limpa os cards antigos
 
-            resultado.forEach(quarto => {
+            // Cria apenas dois cards
+            for (var i = 0; i < 2 && i < resultado.length; i++) {
+                var quarto = resultado[i];
                 var quartoDiv = document.createElement("div");
                 quartoDiv.className = "quarto";
 
+
                 quartoDiv.innerHTML = `
                     <div class="quarto_card">
-                        <img src="../assets/img/Hotel_Pernambuco.png" alt="${quarto.nome}" class="quarto_imagem">
+                        <img src="../assets/img/Cama_do_hotel.png" alt="${quarto.nome}" class="quarto_imagem">
                         <div class="quarto_detalhes">
-                            <h2 class="quarto_nome">${quarto.nome}</h2>
-                            <p class="quarto_camas">${quarto.camas}</p>
-                            <p class="quarto_preco">R$ ${precoTotal}</p>
-                            <button class="quarto_button">Reservar Quarto</button>
+                            <div class="quarto_info">
+                                <h2 class="quarto_nome">${quarto.nome}</h2>
+                                <div class="quarto_camas">${quarto.camas}</div>
+                            </div>
+                            <div class="quarto_preco_info">
+                                <p class="titulo_quarto_preco">Valor Total</p>
+                                <p class="quarto_preco">R$ ${precoTotal}</p>
+                            </div>
+                            <button class="quarto_button" ">Reservar Quarto</button>
                         </div>
                     </div>
                 `;
 
                 resultadoBuscaDiv.appendChild(quartoDiv);
-            });
+            }
         } else {
             alert("Por favor, preencha todos os campos antes de buscar.");
         }
