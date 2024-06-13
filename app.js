99538a61bd026b7b34e8749170b54eab00470065
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-06-12 23:17:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ const express = require('express');
 const port = 8080;
 const app = express();
 
-<<<<<<< Updated upstream
 const router = require('./backend/src/routes/router')
-=======
 app.use(express.json());
-
 
 app.use(express.static('public'));
->>>>>>> Stashed changes
-
-app.use(express.json());
-
-app.use(express.static("public"));
 
 app.use("/", router);
 
